21f38c7caceba75193a2e7904f950b3a8802fa9d
hhoo1645 <43479514+hhoo1645@users.noreply.github.com>
2019-05-31 16:18:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,14 +22,14 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if(message.content === '-مبلغ'){
-        message.channel.send('#credits')
+    if(message.content === '-حب'){
+        message.channel.send('احبببك نيفووووو @XNefoo|Huler#8444 @XNefoo|Huler#8444 @XNefoo|Huler#8444')
     }
 });
 
 client.on('message', message => {
     if(message.content === '-ريب'){
-        message.channel.send('#credits')
+        message.channel.send('#rep @XNefoo|Huler#8444')
     }
 });
 
@@ -40,14 +40,14 @@ client2.on('message', message => {
 });
 
 client2.on('message', message => {
-    if(message.content === '-مبلغ'){
-        message.channel.send('#credits')
+    if(message.content === '-حب'){
+        message.channel.send('احبببك نيفووووو @XNefoo|Huler#8444 @XNefoo|Huler#8444 @XNefoo|Huler#8444')
     }
 });
 
 client2.on('message', message => {
     if(message.content === '-ريب'){
-        message.channel.send('#credits')
+        message.channel.send('#rep @XNefoo|Huler#8444')
     }
 });
 
@@ -58,7 +58,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
@@ -72,7 +72,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
           .then(m => {
             count++;
           })
